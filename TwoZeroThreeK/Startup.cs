15a53223074460d9f3fecaf7b65a8b4f98a6180d
raghavendra-rashi <raghavendra.rashi@gmail.com>
2015-08-20 17:12:29 +0530

    Test Commit

--- TwoZeroThreeK/Startup.cs
+++ TwoZeroThreeK/Startup.cs
@@ -12,7 +12,7 @@ namespace TwoZeroThreeK
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app); 
         }
     }
 }
